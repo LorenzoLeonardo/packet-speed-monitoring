3f7fb4b2b50e52b4c708aa8c3cac189e8f9eda6e
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2025-10-15 22:31:03 +0800

    Implement DeviceInfo
    The purpose of this is to be able to Serialize the information
    to front-end and able to convert back to Device

---
+++ src/device.rs
@@ -0,0 +1,109 @@
+use std::net::{IpAddr, Ipv4Addr};
+
+use anyhow::{Context, Result};
+use async_pcap::{ConnectionStatus, Device};
+use serde::Serialize;
+
+use crate::helpers;
+
+#[derive(Serialize, Debug, Clone)]
+pub struct DeviceInfo {
+    name: String,
+    desc: String,
+    device_ip: Ipv4Addr,
+    network_ip: Ipv4Addr,
+    netmask: Ipv4Addr,
+}
+
+impl TryFrom<DeviceInfo> for Device {
+    type Error = anyhow::Error;
+
+    fn try_from(value: DeviceInfo) -> Result<Self, Self::Error> {
+        let devices = Device::list().map_err(|err| anyhow::anyhow!(err))?;
+
+        devices
+            .iter()
+            .find(|dev| dev.name == value.name)
+            .cloned()
+            .context("Device not found")
+    }
+}
+
+impl TryFrom<Device> for DeviceInfo {
+    type Error = anyhow::Error;
+
+    fn try_from(device: Device) -> Result<Self, Self::Error> {
+        for address in device.addresses.iter() {
+            if address.addr.is_ipv4()
+                && let IpAddr::V4(device_ip) = address.addr
+                && let IpAddr::V4(netmask) = address.netmask.context("Subnet mask not found")?
+            {
+                let network_ip = helpers::network_address(device_ip, netmask);
+                return Ok(Self {
+                    name: device.name,
+                    desc: device.desc.unwrap_or_default(),
+                    device_ip,
+                    network_ip,
+                    netmask,
+                });
+            }
+        }
+        Err(anyhow::anyhow!(
+            "No valid IPv4 subnet found on device {}",
+            device.name
+        ))
+    }
+}
+
+impl DeviceInfo {
+    pub fn get_physical_device() -> Option<DeviceInfo> {
+        Self::find_connected_devices()
+            .iter()
+            .find(|dev| {
+                let octets = dev.device_ip.octets();
+                let is_physical_private =
+                    (octets[0] == 10) || (octets[0] == 192 && octets[1] == 168);
+
+                is_physical_private
+                    && !dev.device_ip.is_loopback()
+                    && !dev.device_ip.is_multicast()
+                    && !dev.device_ip.is_unspecified()
+            })
+            .cloned()
+    }
+    pub fn find_connected_devices() -> Vec<DeviceInfo> {
+        // Get the list of devices, return an empty Vec if listing fails
+        let devices = match Device::list() {
+            Ok(devs) => devs,
+            Err(_) => return Vec::new(),
+        };
+
+        // Filter all devices that meet the given conditions
+        let filtered: Vec<DeviceInfo> = devices
+            .into_iter()
+            .filter(|d| {
+                d.flags.connection_status == ConnectionStatus::Connected
+                    && d.addresses.iter().any(|item| {
+                        if let IpAddr::V4(ipv4) = item.addr {
+                            item.netmask.is_some()
+                                && item.broadcast_addr.is_some()
+                                && !ipv4.is_loopback()
+                                && !ipv4.is_multicast()
+                                && !ipv4.is_unspecified()
+                        } else {
+                            false
+                        }
+                    })
+            })
+            .filter_map(|device| match DeviceInfo::try_from(device) {
+                Ok(info) => Some(info),
+                Err(err) => {
+                    log::warn!("Skipping device due to error: {err}");
+                    None
+                }
+            })
+            .collect();
+        log::info!("Filtered connected devices: {filtered:?}");
+        filtered
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod device;
 mod helpers;
 mod listener;
 mod logger;
